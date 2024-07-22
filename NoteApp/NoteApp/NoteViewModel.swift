0453844d1a9a029b0efc935e78fd9058d5ad3ffe
Jihye Park <deviloper.park@gmail.com>
2024-07-22 13:37:06 +0900

    NoteViewModel에 fetchData 함수 추가, list 출력

--- NoteApp/NoteApp/NoteViewModel.swift
+++ NoteApp/NoteApp/NoteViewModel.swift
@@ -13,8 +13,24 @@ class NoteViewModel: ObservableObject {
     
     private var databaseReference = Firestore.firestore().collection("Notes")
     
+    // 데이터 삽입
     func addData(title: String) {
         let docRef = databaseReference.addDocument(data: ["title": title])
         dump(docRef)
+    }
+    
+    // 데이터 조회
+    func fetchData() {
+        databaseReference.addSnapshotListener { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("No Documents")
+                return
+            }
+            
+            self.notes = documents.compactMap { docSnap -> Note? in
+                return try? docSnap.data(as: Note.self)
+            }
+            
+        }
     }
 }
