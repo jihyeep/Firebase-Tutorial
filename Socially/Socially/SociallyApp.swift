6baaf0c50f2385e82a85b0e19de92f71a892be33
Jihye Park <deviloper.park@gmail.com>
2024-07-23 13:34:49 +0900

    Initialize SociallyApp

---
+++ Socially/Socially/SociallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SociallyApp.swift
+//  Socially
+//
+//  Created by 박지혜 on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SociallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
