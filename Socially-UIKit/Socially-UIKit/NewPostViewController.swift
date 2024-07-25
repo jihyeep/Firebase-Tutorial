a092deee041fc174527b1155c8eefe2ac71dcf8b
Jihye Park <deviloper.park@gmail.com>
2024-07-25 17:06:58 +0900

    이미지 썸네일 저장

--- Socially-UIKit/Socially-UIKit/NewPostViewController.swift
+++ Socially-UIKit/Socially-UIKit/NewPostViewController.swift
@@ -163,15 +163,28 @@ class NewPostViewController: UIViewController, UITextViewDelegate, PHPickerViewC
 
     // MARK: - Methods
     func addData(description: String, datePublished: Date, data: Data, completion: @escaping (Error?) -> Void) {
-        let path = "posts/\(UUID().uuidString).jpg"
+        let path = UUID().uuidString
         let fileRef = storage.child(path)
 
+        // 파일 타입 및 MIME 타입 추측
+        let mimeType = detectMimeType(from: data)
+
+        // 메타데이터 설정
+        let metadata = StorageMetadata()
+        metadata.contentType = mimeType
+        
         fileRef.putData(data, metadata: nil) { metadata, error in
             if let error = error {
                 completion(error)
                 return
             }
-
+            
+            // 임시 처리
+            Thread.sleep(forTimeInterval: 10)
+            
+            // 썸네일 저장
+            let thumbRef = Storage.storage().reference().child("thumbs/\(path)_320x200")
+            
             fileRef.downloadURL { url, error in
                 if let error = error {
                     completion(error)
@@ -185,14 +198,39 @@ class NewPostViewController: UIViewController, UITextViewDelegate, PHPickerViewC
 
                 let post = [
                     "description": description,
-                    "datePublished": Timestamp(date: datePublished),
-                    "imageUrl": url.absoluteString
+                    "datePublished": datePublished,
+                    "imageURL": url.absoluteString
                 ]
 
                 self.db.collection("Posts").addDocument(data: post) { error in
                     completion(error)
                 }
             }
+        }
+    }
+    
+    func detectMimeType(from data: Data) -> String {
+        var c: UInt8 = 0
+        data.copyBytes(to: &c, count: 1)
+
+        switch c {
+        case 0xFF:
+            return "image/jpeg"
+        case 0x89:
+            return "image/png"
+        case 0x47:
+            return "image/gif"
+        case 0x49, 0x4D:
+            return "image/tiff"
+        case 0x25:
+            return "application/pdf"
+        case 0xD0:
+            return "application/vnd"
+        case 0x46:
+            return "text/plain"
+        default:
+            // 기본값으로 application/octet-stream 사용
+            return "application/octet-stream"
         }
     }
 }
