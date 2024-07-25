301c51eb24fa5a7895e5113a5fb36ee5714adb36
Jihye Park <deviloper.park@gmail.com>
2024-07-25 11:09:16 +0900

    Firebase 초기화 코드, Post 모델 추가

---
+++ Socially-UIKit/Socially-UIKit/Post.swift
@@ -0,0 +1,19 @@
+//
+//  Post.swift
+//  Socially-UIKit
+//
+//  Created by 박지혜 on 7/25/24.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Post: Identifiable, Decodable {
+    // Firestore와 id를 mapping
+    @DocumentID var id: String?
+    var description: String?
+    var imageURL: String?
+    
+    // 서버 시간 정렬
+    @ServerTimestamp var datePublished: Date?
+}
